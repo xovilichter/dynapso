173deec6aa3fe91c8ead625e4bf46b7b7c928b9a
root <root@vps67145.ovh.net>
2014-05-21 18:54:22 +0200

    Automatic Commit

--- xovilichter_dynapso_scraper.php
+++ xovilichter_dynapso_scraper.php
@@ -1,40 +1,59 @@
 <?php 
 
-$ch = curl_init(); 
-curl_setopt($ch, CURLOPT_URL, "http://www.dynapso.de/xovilichter/"); 
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-curl_setopt($ch, CURLOPT_USERAGENT, 'Xovilichter.Dynapso/1.0 (+https://github.com/xovilichter/dynapso)');
-curl_setopt($ch, CURLOPT_REFERER, 'https://github.com/xovilichter/dynapso'); 
-$output = curl_exec($ch);
-curl_close($ch);
+// set default time zone
+date_default_timezone_set('Europe/Berlin');
 
-preg_match_all('#<td class="right"><span data-sort="(.*)">(.*)</span></td> <td class="link"><span data-sort="(.*)">(.*)</span></td> <td class="googlePlus">(.*)</td>#Uis', $output, $matches);
+function xovilichter_dynapso_scraper(){
 
-$search_results = array();
+	// set default time zone
+	date_default_timezone_set('Europe/Berlin');
 
-foreach($matches[1] as $n => $position){
-
-	if(preg_match('#<img class="icon" src="http://www.dynapso.de/images/icons/(.*).png" alt="(.*)"#Uis', $matches[2][$n], $universal_search_match)){
-		$type = $universal_search_match[1];		
-	}else{
-		$type = 'organic';
+	// curl
+	$ch = curl_init(); 
+	curl_setopt($ch, CURLOPT_URL, "http://www.dynapso.de/xovilichter/"); 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+	curl_setopt($ch, CURLOPT_USERAGENT, 'Xovilichter.Dynapso/1.0 (+https://github.com/xovilichter/dynapso)');
+	curl_setopt($ch, CURLOPT_REFERER, 'https://github.com/xovilichter/dynapso'); 
+	$output = curl_exec($ch);
+	curl_close($ch);
+	
+	// last update
+	preg_match('#>letztes Update: (.*)h<#Uis', $output, $last_update_match);
+	$last_update = date('Y-m-d H:i', strtotime($last_update_match[1]));
+	
+	// match results
+	preg_match_all('#<td class="right"><span data-sort="(.*)">(.*)</span></td> <td class="link"><span data-sort="(.*)">(.*)</span></td> <td class="googlePlus">(.*)</td>#Uis', $output, $matches);
+	
+	$search_results = array();
+	
+	foreach($matches[1] as $n => $position){
+	
+		if(preg_match('#<img class="icon" src="http://www.dynapso.de/images/icons/(.*).png" alt="(.*)"#Uis', $matches[2][$n], $universal_search_match)){
+			$type = $universal_search_match[1];		
+		}else{
+			$type = 'organic';
+		}
+		
+		if(preg_match('#<a href="(.*)" title="(.*)" target="_blank"><img class="ico" src="(.*)" alt="(.*)"#Uis', $matches[5][$n], $google_plus_match)){
+			$google_plus = array(
+				'name' => $google_plus_match[4],
+				'short_url' => $google_plus_match[1],
+			);
+		}else{
+			$google_plus = array();
+		}
+	
+		$search_results[] = array(
+			'position' => $position,
+			'url' => $matches[3][$n],
+			'type' => $type,
+			'google_plus' => $google_plus,
+		);
 	}
 	
-	if(preg_match('#<a href="(.*)" title="(.*)" target="_blank"><img class="ico" src="(.*)" alt="(.*)"#Uis', $matches[5][$n], $google_plus_match)){
-		$google_plus = array(
-			'name' => $google_plus_match[4],
-			'short_url' => $google_plus_match[1],
-		);
-	}else{
-		$google_plus = array();
-	}
-
-	$search_results[] = array(
-		'position' => $position,
-		'url' => $matches[3][$n],
-		'type' => $type,
-		'google_plus' => $google_plus,
+	return array(
+		'last_update' => $last_update,
+		'search_results' => $search_results,
 	);
+	
 }
-
-print_r($search_results);
